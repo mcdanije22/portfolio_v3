7d34639348f58174fc6a564c9687700139145f61
mcdanije22 <mcdanije22@gmail.com>
2020-05-30 22:53:26 -0400

    background image fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Project from "../components/Project/Project"
-import Img from "gatsby-image"
 import "./index.scss"
-import test from "../../public/h1.jpg"
 import {
   MailOutlined,
   GithubOutlined,
@@ -12,6 +10,7 @@ import {
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import BackgroundImage from "gatsby-background-image"
 
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
@@ -34,83 +33,85 @@ const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Josh McDaniel" />
-      <div id="landingPage">
-        <div id="landingContent">
-          <div id="headlineText">
-            <h1>Hi,</h1>
-            <h1>
-              I'm <span style={{ color: "#ff4b36" }}>Josh</span>
-            </h1>
-            <h3>Full-Stack Developer</h3>
-            <a
-              href="https://drive.google.com/file/d/11uWDnXEIVM3N3JeBbtLdgKeDbF-ZlQ9z/view"
-              target="_blank"
-            >
-              <button type="submit">Resume</button>
-            </a>
+      <BackgroundImage fluid={data.allFile.edges[3].node.childImageSharp.fluid}>
+        <div id="landingPage">
+          <div id="landingContent">
+            <div id="headlineText">
+              <h1>Hi,</h1>
+              <h1>
+                I'm <span style={{ color: "#ff4b36" }}>Josh</span>
+              </h1>
+              <h3>Full-Stack Developer</h3>
+              <a
+                href="https://drive.google.com/file/d/11uWDnXEIVM3N3JeBbtLdgKeDbF-ZlQ9z/view"
+                target="_blank"
+              >
+                <button type="submit">Resume</button>
+              </a>
+            </div>
+            <ul id="bottomSocialBar">
+              <li className="socialItem">
+                <a href="mailto:mcdanije22@gmail.com?Subject=Hello%20again">
+                  <button
+                    type="submit"
+                    style={{
+                      borderRadius: "2.5rem",
+                      width: "2.5rem",
+                      height: "2.5rem",
+                      lineHeight: "2.3rem",
+                      backgroundColor: "transparent",
+                      color: "white",
+                      textAlign: "center",
+                      border: "1px solid white",
+                      marginLeft: "0",
+                    }}
+                  >
+                    <MailOutlined />
+                  </button>
+                </a>
+              </li>
+              <li className="socialItem">
+                <a href="https://github.com/mcdanije22" target="_blank">
+                  <button
+                    type="submit"
+                    style={{
+                      borderRadius: "2.5rem",
+                      width: "2.5rem",
+                      height: "2.5rem",
+                      lineHeight: "2.3rem",
+                      backgroundColor: "transparent",
+                      color: "white",
+                      textAlign: "center",
+                      border: "1px solid white",
+                    }}
+                  >
+                    <GithubOutlined />
+                  </button>
+                </a>
+              </li>
+              <li className="socialItem">
+                <a href="https://www.linkedin.com/in/mcdanije/" target="_blank">
+                  <button
+                    type="submit"
+                    style={{
+                      borderRadius: "2.5rem",
+                      width: "2.5rem",
+                      height: "2.5rem",
+                      lineHeight: "2.3rem",
+                      backgroundColor: "transparent",
+                      color: "white",
+                      textAlign: "center",
+                      border: "1px solid white",
+                    }}
+                  >
+                    <LinkedinOutlined />
+                  </button>
+                </a>
+              </li>
+            </ul>
           </div>
-          <ul id="bottomSocialBar">
-            <li className="socialItem">
-              <a href="mailto:mcdanije22@gmail.com?Subject=Hello%20again">
-                <button
-                  type="submit"
-                  style={{
-                    borderRadius: "2.5rem",
-                    width: "2.5rem",
-                    height: "2.5rem",
-                    lineHeight: "2.3rem",
-                    backgroundColor: "transparent",
-                    color: "white",
-                    textAlign: "center",
-                    border: "1px solid white",
-                    marginLeft: "0",
-                  }}
-                >
-                  <MailOutlined />
-                </button>
-              </a>
-            </li>
-            <li className="socialItem">
-              <a href="https://github.com/mcdanije22" target="_blank">
-                <button
-                  type="submit"
-                  style={{
-                    borderRadius: "2.5rem",
-                    width: "2.5rem",
-                    height: "2.5rem",
-                    lineHeight: "2.3rem",
-                    backgroundColor: "transparent",
-                    color: "white",
-                    textAlign: "center",
-                    border: "1px solid white",
-                  }}
-                >
-                  <GithubOutlined />
-                </button>
-              </a>
-            </li>
-            <li className="socialItem">
-              <a href="https://www.linkedin.com/in/mcdanije/" target="_blank">
-                <button
-                  type="submit"
-                  style={{
-                    borderRadius: "2.5rem",
-                    width: "2.5rem",
-                    height: "2.5rem",
-                    lineHeight: "2.3rem",
-                    backgroundColor: "transparent",
-                    color: "white",
-                    textAlign: "center",
-                    border: "1px solid white",
-                  }}
-                >
-                  <LinkedinOutlined />
-                </button>
-              </a>
-            </li>
-          </ul>
         </div>
-      </div>
+      </BackgroundImage>
       <div id="projectsContainer">
         <div id="projectHeader">
           <h1>Projects</h1>
@@ -119,7 +120,7 @@ const IndexPage = () => {
         <div id="projectList">
           <Project
             title="Macros-Social Media App"
-            img={data.allFile.edges[4].node.childImageSharp.fluid}
+            img={data.allFile.edges[5].node.childImageSharp.fluid}
             tech="HTML | CSS | TypeScript | Next js/ React js | Context API | Node js | MongoDB"
             description="A web application that lets users create, find, and share macro-friendly meals with friends. Creating a food post lets a user upload an image as well as the ability to search a food database for ingredients to easily attach nutrition values to the post. Use email: test@gmail.com and password: 12345678 as a test account. Mobile-first design patterns."
             codeLink="https://github.com/mcdanije22/macros"
@@ -128,7 +129,7 @@ const IndexPage = () => {
           />
           <Project
             title="The Nerd Store"
-            img={data.allFile.edges[3].node.childImageSharp.fluid}
+            img={data.allFile.edges[4].node.childImageSharp.fluid}
             tech="HTML | CSS/SASS | Javascript | React js | Gatsby Js | GraphQL | Headless CMS"
             description="A web application that was created to be highly responsive, fast and optimized for SEO using Gatsby js. This was utilized to increase the traffic of a local gaming shop. Created using a headless cms, Contentful, for easy event editing and focusing on quick user accessibility. Provided users with access to shop event info, twitter announcements, photos, and contact/message abilities. Mobile-first design patterns."
             codeLink="https://github.com/mcdanije22/nerd_store"
